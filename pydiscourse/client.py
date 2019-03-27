c0566f2aadc4f9b3474288b3b4b4d8e2b4cf3801
Raphaël Voyazopoulos <raphael.voyazopoulos@ircam.fr>
2019-03-27 18:14:14 +0100

    Handling case where server returns empty `errors` prop

--- pydiscourse/client.py
+++ pydiscourse/client.py
@@ -1468,8 +1468,8 @@ class DiscourseClient(object):
         except ValueError:
             raise DiscourseError("failed to decode response", response=response)
 
-        if "errors" in decoded:
-            message = decoded.get("message")
+        if 'errors' in decoded and len(decoded['errors']) > 0:
+            message = decoded.get('message')
             if not message:
                 message = u",".join(decoded["errors"])
             raise DiscourseError(message, response=response)
