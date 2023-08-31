6c1d9a9e1d7dbb5e00bf99c05df6efe3a7718322
Ben Lopatin <ben@benlopatin.com>
2023-08-31 17:21:05 -0400

    Use data from fixture
    Makes it explicit that this data is used here, plus makes the line
    shorter.

--- tests/conftest.py
+++ tests/conftest.py
@@ -47,8 +47,8 @@ def email():
 
 
 @pytest.fixture(scope="session")
-def redirect_url():
-    return "/session/sso_login?sso=bm9uY2U9Y2I2ODI1MWVlZmI1MjExZTU4YzAwZmYxMzk1ZjBjMGImbmFtZT1z%0AYW0mdXNlcm5hbWU9c2Ftc2FtJmVtYWlsPXRlc3QlNDB0ZXN0LmNvbSZleHRl%0Acm5hbF9pZD1oZWxsbzEyMw%3D%3D%0A&sig=1c884222282f3feacd76802a9dd94e8bc8deba5d619b292bed75d63eb3152c0b"
+def redirect_url(sso_payload):
+    return f"/session/sso_login?sso={sso_payload}YW0mdXNlcm5hbWU9c2Ftc2FtJmVtYWlsPXRlc3QlNDB0ZXN0LmNvbSZleHRl%0Acm5hbF9pZD1oZWxsbzEyMw%3D%3D%0A&sig=1c884222282f3feacd76802a9dd94e8bc8deba5d619b292bed75d63eb3152c0b"
 
 
 @pytest.fixture(scope="session")
